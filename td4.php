52c9737492b32f8d99782323674f99ea00398e5c
Mehdi GHOULAM <aznoggfr@gmail.com>
2021-10-21 20:43:26 +0200

    Finish TD4

--- td4.php
+++ td4.php
@@ -5,6 +5,126 @@
   <meta charset="UTF-8" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/highlight.js@11.3.1/styles/github-dark.css" />
   <title>Document</title>
+</head>
+
+<body>
+  <div class="container">
+
+    <form action="" method="POST">
+      <fieldset>
+        <legend>Contactez moi</legend>
+        <div class="form-control">
+          <label for="name">Nom</label>
+          <input type="text" name="name" id="name" placeholder="Entrez votre nom" required autocomplete="off" />
+        </div>
+        <div class="form-control">
+          <label for="email">Email</label>
+          <input type="email" name="email" id="email" placeholder="Entrez votre email" required autocomplete="off" />
+        </div>
+        <div class="form-control">
+          <label for="message">Message</label>
+          <textarea name="message" id="message" cols="30" rows="5" maxlength="300" placeholder="Entrez votre message" required autocomplete="off"></textarea>
+        </div>
+        <input type="submit" value="Envoyer" class="submit-btn" />
+      </fieldset>
+    </form>
+
+    <form action="" method="POST">
+      <fieldset>
+        <legend>Inscription</legend>
+        <!-- 
+          NOM
+         -->
+        <div class="form-control">
+          <label for="name">Nom</label>
+          <input type="text" name="name" id="name" required autocomplete="off" />
+        </div>
+
+        <!-- 
+          MOT DE PASSE
+         -->
+        <div class="form-control">
+          <label for="password">Mot de passe</label>
+          <input type="password" name="password" id="password" required autocomplete="off" />
+        </div>
+
+        <!-- 
+          EMAIL
+         -->
+        <div class="form-control">
+          <label for="email">Email</label>
+          <input type="email" name="email" id="email" required autocomplete="off" />
+        </div>
+
+        <!-- 
+          SEXE
+         -->
+        <div class="form-control">
+          <label for="sexe">Sexe</label>
+          <div class="form-inline">
+            <input type="radio" name="sexe" id="male" style="width: auto;" required>
+            <span>Homme</span>
+            <input type="radio" name="sexe" id="female" style="width: auto;" required>
+            <span>Femme</span>
+          </div>
+        </div>
+
+        <!-- 
+          MOBILE
+         -->
+        <div class="form-control">
+          <label for="mobile">Mobile</label>
+          <input type="text" name="mobile" id="mobile" required autocomplete="off" />
+        </div>
+
+        <!-- 
+          DATE DE NAISSANCE
+         -->
+        <div class="form-control">
+          <label for="birth">Date de naissance</label>
+          <input type="date" name="birth" id="birth" required>
+        </div>
+
+        <!-- 
+          NOMBRE ANIMAUX
+         -->
+        <div class="form-control">
+          <label for="no_animal">Nombre d'animaux de compagnie</label>
+          <div class="form-inline">
+            <input type="range" name="no_animal" id="no_animal" min="0" max="10" value="0">
+            <span id="no_animal_value">0</span>
+            <script>
+              document.querySelector("#no_animal").addEventListener('input', () => {
+                document.querySelector("#no_animal_value").innerHTML = document.querySelector("#no_animal").value;
+              });
+            </script>
+          </div>
+        </div>
+
+        <!-- 
+          PREMIER ANIMAL
+         -->
+        <div class="form-control">
+          <label for="first_animal">Premier animal possédé</label>
+          <input type="text" name="first_animal" id="first_animal">
+        </div>
+
+
+        <!-- ENVOYER -->
+        <input type="submit" value="Envoyer" class="submit-btn" />
+      </fieldset>
+    </form>
+    <pre style="padding: 50px 0;">
+        <code style="text-align: start;"><?php
+                                          echo htmlspecialchars(preg_replace("/<pre((.|\n)*)script>/i", "", file_get_contents(__file__)));
+                                          ?></code>
+      </pre>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/highlight.min.js"></script>
+    <script>
+      hljs.highlightAll();
+    </script>
+  </div>
+
   <style>
     @import url("https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap");
 
@@ -23,6 +143,29 @@
       place-content: center;
     }
 
+    form {
+      padding-top: 50px;
+      width: fit-content;
+      margin: 0 auto;
+    }
+
+    .form-control {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      padding-bottom: 15px;
+    }
+
+    .form-inline {
+      display: flex;
+      width: -webkit-fill-available;
+      align-items: center;
+    }
+
+    .form-inline span {
+      padding: 0 10px;
+    }
+
     input[type="submit"] {
       width: 100%;
       text-decoration: none;
@@ -37,16 +180,9 @@
       background-color: white;
     }
 
-    .form-control {
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
-      padding-bottom: 15px;
-    }
-
     .form-control input,
     textarea {
-      width: 60%;
+      width: 100%;
       padding: 5px;
       font-family: "JetBrains Mono", sans-serif;
       color: #1a1e21;
@@ -57,48 +193,13 @@
     }
 
     fieldset {
-      padding: 15px;
+      padding: 15px 25px;
     }
 
     legend {
       padding: 0 10px;
     }
-
-    code {
-      text-align: start;
-    }
   </style>
-</head>
-
-<body>
-  <form action="">
-    <fieldset>
-      <legend>Contactez moi</legend>
-
-      <div class="form-control">
-        <label for="name">Nom</label>
-        <input type="text" name="name" id="name" placeholder="Entrez votre nom" required autocomplete="off" />
-      </div>
-      <div class="form-control">
-        <label for="email">Email</label>
-        <input type="text" name="email" id="email" placeholder="Entrez votre email" required autocomplete="off" />
-      </div>
-      <div class="form-control">
-        <label for="message">Message</label>
-        <textarea name="message" id="message" cols="30" rows="5" placeholder="Entrez votre message" required autocomplete="off"></textarea>
-      </div>
-      <input type="submit" value="Envoyer" class="submit-btn" />
-    </fieldset>
-  </form>
-  <pre>
-      <code><?php
-            echo htmlspecialchars(preg_replace("/<pre((.|\n)*)script>/i", "", file_get_contents(__file__)));
-            ?></code>
-    </pre>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/highlight.min.js"></script>
-  <script>
-    hljs.highlightAll();
-  </script>
 </body>
 
 </html>
